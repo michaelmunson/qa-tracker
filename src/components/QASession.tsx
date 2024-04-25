3561d121a98f25d549a76ae7968a7c9ea072a2a0
Michael Munson <michael.s.munson@outlook.com>
2024-04-24 23:49:23 -0400

    first commit

---
+++ src/components/QASession.tsx
@@ -0,0 +1,226 @@
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import {
+    Box,
+    Stepper,
+    Step,
+    StepLabel,
+    StepContent,
+    Button,
+    Paper,
+    Typography,
+    Stack,
+    LinearProgress,
+    TextField
+} from "@mui/material"
+import "./qa-session.css"
+
+type Grade = "passed" | "failed" | "partial"
+
+interface Instructions {
+    [key: string]: (
+        string[] | Instructions
+    )
+}
+
+interface QAResult {
+    grade: Grade
+    reason?: string
+}
+
+
+function QAInstruction({
+    instruction,
+    result,
+    setResult
+}: {
+    instruction: string
+    result?: QAResult
+    setResult: React.Dispatch<React.SetStateAction<QAResult | undefined>>
+}) {
+    const getVariant = useCallback((conditional: boolean) => conditional ? "contained" : "outlined", [])
+
+    return (
+        <Stack>
+            <Typography>{instruction}</Typography>
+            <Stack direction={"row"} spacing={2}>
+                <Button variant={getVariant(result?.grade === "failed")} color="error" onClick={() => setResult(prev => ({ ...prev, grade: "failed" }))}> Failed </Button>
+                <Button variant={getVariant(result?.grade === "partial")} color="warning" onClick={() => setResult(prev => ({ ...prev, grade: "partial" }))}> Partial </Button>
+                <Button variant={getVariant(result?.grade === "passed")} color="success" onClick={() => setResult(prev => ({ ...prev, grade: "passed" }))}> Success </Button>
+            </Stack>
+        </Stack>
+    )
+}
+
+
+function QAInstructionList({
+    instructionList,
+    handleNext
+}: {
+    instructionList: string[],
+    handleNext: (results: QAResult[]) => void
+}) {
+    const [qaResults, setQaResults] = useState<(QAResult | null)[]>(instructionList.map(x => null));
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [result, setResult] = useState<QAResult>();
+    const activeInstruction = useMemo(() => instructionList[activeIndex], [activeIndex])
+    const getVariant = useCallback((conditional: boolean) => conditional ? "contained" : "outlined", []);
+    const progressPercent = useMemo(() => Math.round((qaResults.filter(x => !!x).length / instructionList.length) * 100), [qaResults])
+
+    useEffect(() => {
+        if (result?.grade === "passed") {
+            setQaResults(arr => {
+                const newArr = [...arr];
+                newArr[activeIndex] = result;
+                return newArr
+            });
+            setResult(undefined);
+            setActiveIndex(act => act + 1);
+        }
+    }, [result]);
+
+    useEffect(() => progressPercent === 100 ? handleNext(qaResults as QAResult[]) : undefined, [progressPercent])
+
+    return (
+        <div className="qa-instruction-list">
+            <div className="progress">
+                <p>{progressPercent}% Complete</p>
+                <LinearProgress variant="determinate" value={progressPercent} style={{ borderRadius: "10px", height: '5px', width: '100%' }} />
+            </div>
+            <div className="qa-instruction">
+                <h3>{activeInstruction}</h3>
+                <div className="grade-buttons">
+                    <Button variant={getVariant(result?.grade === "failed")} color="error" onClick={() => setResult(prev => ({ ...prev, grade: "failed" }))}> Failed </Button>
+                    <Button variant={getVariant(result?.grade === "partial")} color="warning" onClick={() => setResult(prev => ({ ...prev, grade: "partial" }))}> Partial </Button>
+                    <Button variant={getVariant(result?.grade === "passed")} color="success" onClick={() => setResult(prev => ({ ...prev, grade: "passed" }))}> Passed </Button>
+                </div>
+                <div 
+                    style={{
+                        display:"flex",
+                        flexDirection: "column",
+                        gap:"10px",
+                        overflow: 'hidden',
+                        maxHeight: (result?.grade && result.grade !== "passed") ? "200px" : "0px",
+                        transition: '500ms'
+                    }}>
+                    <TextField
+                        key={`fail-reason-${activeIndex}`}
+                        helperText={`What is the reason for the ${result?.grade.toUpperCase()} grade? Please be specific and include as many details as possible.`}
+                        placeholder={`Reason for ${result?.grade.toUpperCase()} grade?`}
+                        onChange={e => setResult(prev => ({...prev, reason: e.target.value}) as QAResult)}
+                        multiline
+                        rows={4}
+                    />
+                    <Button disabled={!result?.reason} variant="outlined" onClick={() => {
+                        setQaResults(arr => {
+                            const newArr = [...arr];
+                            newArr[activeIndex] = result as QAResult;
+                            return newArr
+                        });
+                        setResult(undefined);
+                        setActiveIndex(act => act + 1);
+                    }}> Continue </Button>
+                </div>
+
+                {/* {result?.grade && result.grade !== "passed" ? (
+                    
+                ) : (
+                    <></>
+                )} */}
+            </div>
+        </div>
+    )
+}
+
+function QAInstructionSet({
+    instructions,
+    level = 0,
+    handleNext
+}: {
+    instructions: Instructions,
+    level?: number
+    handleNext: (results: Record<string, any>) => void
+}) {
+    const [qaResults, setQaResults] = useState<Record<string, any>>(
+        Object.entries(instructions).reduce((acc, [key, val]) => ({ ...acc, [key]: null }), {})
+    )
+
+    const [activeStep, setActiveStep] = React.useState(0);
+
+    useEffect(() => {
+        if ((Object.values(qaResults).filter(x => !!x).length === Object.values(instructions).length)) {
+            handleNext(qaResults);
+        }
+    }, [qaResults])
+
+    return (
+        <div className="qa-instruction-set">
+            <Stepper activeStep={activeStep} orientation="vertical">
+                {Object.entries(instructions).map(([label, value], index) => {
+                    if (Array.isArray(value)) return (
+                        <Step key={`${label.replaceAll(' ', '')}-${index}-key`}>
+                            <StepLabel>
+                                {label}
+                                {/* <Typography style={{fontSize: `${1 - (level * .1)}rem`}}>{label}</Typography> */}
+                            </StepLabel>
+                            <StepContent>
+                                <QAInstructionList
+                                    instructionList={value}
+                                    handleNext={(results: QAResult[]) => {
+                                        setQaResults(prev => ({
+                                            ...prev,
+                                            ...{ [label]: results }
+                                        }))
+                                        setActiveStep(p => index + 1);
+                                    }} />
+                            </StepContent>
+                        </Step>
+                    )
+                    else return (
+                        <Step key={`${label.replaceAll(' ', '')}-${index}-key`}>
+                            <StepLabel>
+                                {label}
+                                {/* <Typography style={{fontSize: `${1 - (level * .1)}rem`, fontWeight:"bold"}}>{label}</Typography> */}
+                            </StepLabel>
+                            <StepContent>
+                                <QAInstructionSet
+                                    instructions={value}
+                                    level={level + 1}
+                                    handleNext={(results: Record<string, any>) => {
+                                        setQaResults(prev => ({
+                                            ...prev,
+                                            ...{ [label]: results }
+                                        }))
+                                        setActiveStep(index + 1);
+                                    }}
+                                />
+                            </StepContent>
+                        </Step>
+                    );
+                })}
+            </Stepper>
+        </div>
+    );
+}
+
+export default function QASession({
+    instructions
+}: {
+    instructions: Instructions
+}) {
+    const [qaResults, setQaResults] = useState<Record<string, any>>();
+
+    useEffect(() => {
+        if (qaResults) console.log(qaResults);
+    }, [qaResults])
+
+    return (
+        <div id="qa-session">
+            <QAInstructionSet
+                instructions={instructions}
+                handleNext={results => {
+                    setQaResults(results)
+                }} />
+        </div>
+    )
+}
+
---
+++ src/mock.data.ts
@@ -0,0 +1,19 @@
+export default {
+    "Login": {
+      "Username/Password": [
+        "Enter incorrect username/password and verify error message shows up",
+        "Enter correct password, verify button shows spinner"
+      ],
+      "OTP": [
+        "Enter incorrect OTP, verify that error message is displayed",
+        "Enter correct OTP, verify login is successful"
+      ],
+    },
+    "Cases List": [
+      "Click a case and ensure that it goes to new case",
+      "Click new recording and verify that it goes to new case"
+    ],
+    "New Recording": [
+      "click new recording button and verify recording starts"
+    ]
+}
